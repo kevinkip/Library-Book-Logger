ee49ab0e961d553dbe406fde663c31582afe5afc
Kevin Kipkemboi <4940465+kevinkip@users.noreply.github.com>
2023-04-06 16:40:35 -0700

    added popup form

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const list = document.getElementById('bookList');
+const btn = document.querySelector(".newBook");
+const span = document.querySelector(".close");
+const form = document.querySelector(".addForm");
+
+btn.addEventListener("click", () => {
+   form.style.display = "block"; 
+})
+
+span.addEventListener("click", () => {
+    form.style.display = "none";
+})
+
+window.addEventListener("click", (e)=> {
+    if(e.target == form){ /* if user clicks anywhere outside of form, close it*/
+        form.style.display = "none"
+    }
+})
 
 let bookList = [
     {
@@ -15,9 +32,9 @@ let bookList = [
     }
 ];
 
-for (i in bookList){
-    list.appendChild(bookList[i])
-}
+// for (i in bookList){
+//     list.appendChild(bookList[i])
+// }
 
 function Book(title, author, pages, read){
     this.title = title
